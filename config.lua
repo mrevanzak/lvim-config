3a1a4f4ff6f407c6b4aa663b4f0fb85f0b80f9c0
mrevanzak <mrevanzak@gmail.com>
2023-01-25 12:29:07 +0700

    fix: goto-preview bugs

--- config.lua
+++ config.lua
@@ -227,9 +227,7 @@ lvim.plugins = {
   {
     'rmagatti/goto-preview',
     config = function()
-      require('goto-preview').setup {
-        dismiss_on_move = true,
-      }
+      require('goto-preview').setup()
     end
   },
 
