a5d26a46cbf8d01853e96a46e908234e7c282869
mrevanzak <mrevanzak@gmail.com>
2023-07-21 15:54:17 +0700

    feat: use prettier for rust

--- config.lua
+++ config.lua
@@ -1,3 +1,18 @@
+-- PATCH: in order to address the message:
+-- vim.treesitter.query.get_query() is deprecated, use vim.treesitter.query.get() instead. :help deprecated
+--   This feature will be removed in Nvim version 0.10
+local orig_notify = vim.notify
+local filter_notify = function(text, level, opts)
+  -- more specific to this case
+  if type(text) == "string" and (string.find(text, "get_query", 1, true) or string.find(text, "get_node_text", 1, true)) then
+    -- for all deprecated and stack trace warnings
+    -- if type(text) == "string" and (string.find(text, ":help deprecated", 1, true) or string.find(text, "stack trace", 1, true)) then
+    return
+  end
+  orig_notify(text, level, opts)
+end
+vim.notify = filter_notify
+
 --[[
 lvim is the global options object
 
@@ -188,12 +203,11 @@ formatters.setup {
     command = "prettierd",
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "json", "graphql", "markdown",
       "yaml",
-      "html", "css", "scss", "less", "vue", "svelte" }
+      "html", "css", "scss", "less", "vue", "svelte", "rust" },
   },
 
   { command = "pint",        filetypes = { "php" } },
   { command = "black",       filetypes = { "phyton" } },
-  { command = "rustfmt",     filetypes = { "rust" } },
   -- {
   --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
   --   command = "prettier",
